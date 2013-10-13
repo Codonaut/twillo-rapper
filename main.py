11e36ff1c842df9e5992f36e1c7184a8669f9b7c
Scott Merkling <sbmerkling@gmail.com>
2013-10-12 21:32:39 -0400

    init

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import (Flask, request, session, g, redirect, 
+				   url_for, abort, render_template, flash)
+from pymongo import MongoClient
+
+DEBUG = True
+conn = MongoClient()
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def index():
+	return 'Hello World'
+
+if __name__ == '__main__':
+	app.run()
